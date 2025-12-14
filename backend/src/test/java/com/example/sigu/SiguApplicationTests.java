4c97678745188ffdb9eb7a60c022ef9c87e67116
DiegoMillanOrtega <diegomillan1005@gmail.com>
2025-12-14 10:09:46 -0500

    feat: se agrega el backend

---
+++ backend/src/test/java/com/example/sigu/SiguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sigu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
