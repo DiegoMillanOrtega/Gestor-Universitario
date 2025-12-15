67b7a066966cf65d87d39c6c6772fbd957f5b5f4
DiegoMillanOrtega <diegomillan1005@gmail.com>
2025-12-15 15:11:20 -0500

    feat: se añade dos nuevas consultas. Estas consultas buscar por el id de usuario autenticado y por semestre

--- backend/src/main/java/com/example/sigu/persistence/repository/IMateriaRepository.java
+++ backend/src/main/java/com/example/sigu/persistence/repository/IMateriaRepository.java
@@ -4,7 +4,11 @@ import com.example.sigu.persistence.entity.Materia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface IMateriaRepository extends JpaRepository<Materia,Long> {
-
+    List<Materia> findAllBySemestre_UsuarioId(Long id);
+    Optional<Materia> findByIdAndSemestre_UsuarioId(Long materiaId, Long usuarioId);
 }
