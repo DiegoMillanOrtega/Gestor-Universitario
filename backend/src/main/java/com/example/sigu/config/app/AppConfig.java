c2c9c0c8a3b009985852ab6afab22f5d6ef8e59a
DiegoMillanOrtega <diegomillan1005@gmail.com>
2025-12-14 20:43:38 -0500

    feat: se agrega configuracion de la app

---
+++ backend/src/main/java/com/example/sigu/config/app/AppConfig.java
@@ -0,0 +1,37 @@
+package com.example.sigu.config.app;
+
+import com.example.sigu.service.interfaces.IUsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+
+    private final IUsuarioService usuarioService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailService() {
+        return username -> usuarioService.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not fournd"));
+    }
+}
