5f32762adbfc3f72776dd0368b06c488af8465c0
DiegoMillanOrtega <diegomillan1005@gmail.com>
2025-12-15 15:14:02 -0500

    feat: se añade un dto para las respuesta de error para el cliente

---
+++ backend/src/main/java/com/example/sigu/presentation/dto/ErrorResponse.java
@@ -0,0 +1,9 @@
+package com.example.sigu.presentation.dto;
+
+import java.time.Instant;
+
+public record ErrorResponse(
+        int status,
+        String message,
+        Instant timestamp
+) {}
