168c0b589d5a4fa0ec2fe98d1db5723a6daa3cff
DiegoMillanOrtega <diegomillan1005@gmail.com>
2025-12-21 14:49:35 -0500

    feat: se añade un dto para actualizar las notas

---
+++ backend/src/main/java/com/example/sigu/presentation/dto/nota/NotaUpdateRequest.java
@@ -0,0 +1,29 @@
+package com.example.sigu.presentation.dto.nota;
+
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record NotaUpdateRequest(
+        @DecimalMin(value = "0.0", inclusive = true, message = "La calificación mínima es 0.0.")
+        @DecimalMax(value = "5.0", inclusive = true, message = "La calificación máxima es 5.0.")
+        BigDecimal p1,
+
+        @DecimalMin(value = "0.0", inclusive = true, message = "La calificación mínima es 0.0.")
+        @DecimalMax(value = "5.0", inclusive = true, message = "La calificación máxima es 5.0.")
+        BigDecimal p2,
+
+        @DecimalMin(value = "0.0", inclusive = true, message = "La calificación mínima es 0.0.")
+        @DecimalMax(value = "5.0", inclusive = true, message = "La calificación máxima es 5.0.")
+        BigDecimal p3,
+
+        @DecimalMin(value = "0.0", inclusive = true, message = "La calificación mínima es 0.0.")
+        @DecimalMax(value = "5.0", inclusive = true, message = "La calificación máxima es 5.0.")
+        BigDecimal ex,
+
+        Long materiaId
+) {
+}
